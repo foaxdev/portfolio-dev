2f87ac3bd0246279cb3b63bd0630ea2536e0f124
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-04 22:43:06 +0300

    Adds portfolio data

---
+++ source/data/portfolio.js
@@ -0,0 +1,37 @@
+export const portfolio = {
+  pognali: {
+    sourceLink: "https://github.com/foaxdev/pognali-dev",
+    prodLink: "https://foaxdev.github.io/pognali/",
+    alias: "pognali"
+  },
+  woodenCoyote: {
+    sourceLink: "https://github.com/foaxdev/wooden-coyote-dev",
+    prodLink: "https://wooden-coyote.site",
+    alias: "oden-coyote"
+  },
+  seaCruises: {
+    sourceLink: "https://github.com/foaxdev/sea-cruises-dev",
+    prodLink: "https://foaxdev.github.io/sea-cruises/",
+    alias: "sea-cruises"
+  },
+  mishka: {
+    sourceLink: "https://github.com/foaxdev/mishka-dev",
+    prodLink: "https://foaxdev.github.io/mishka/",
+    alias: "mishka"
+  },
+  pinch: {
+    sourceLink: "https://github.com/foaxdev/pinch-dev",
+    prodLink: "https://foaxdev.github.io/pinch/",
+    alias: "pinch"
+  },
+  gllacy: {
+    sourceLink: "https://github.com/foaxdev/gllacy-dev",
+    prodLink: "https://foaxdev.github.io/gllacy/",
+    alias: "gllacy"
+  },
+  device: {
+    sourceLink: "https://github.com/foaxdev/device-dev",
+    prodLink: "https://foaxdev.github.io/device",
+    alias: "device"
+  }
+};
