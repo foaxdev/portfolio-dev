5543f9bcf6f9ad9067c178f1bacb9b518706aef4
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-04 23:27:07 +0300

    Adds graphic editors data

---
+++ source/data/graphic-editors.js
@@ -0,0 +1,8 @@
+export const graphicEditors = {
+  figma: "Figma",
+  zeplin: "Zeplin",
+  photoshop: "Photoshop",
+  xd: "Xd",
+  affinityPhoto: "Affinity Photo",
+  affinityDesigner: "Affinity Designer"
+};
