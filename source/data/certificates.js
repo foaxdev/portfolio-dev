ebc81b872905fe1b51fb0097fef01804a6b1cd55
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-04 23:28:36 +0300

    Adds certificates data

---
+++ source/data/certificates.js
@@ -0,0 +1,6 @@
+export const certificates = {
+  certificateHtmlCss1: "https://assets.htmlacademy.ru/certificates/intensive/109/673735.pdf?1560923176",
+  certificateHtmlCss2: "https://assets.htmlacademy.ru/certificates/intensive/115/673735.pdf?1565872591",
+  certificatePhp1: "https://assets.htmlacademy.ru/certificates/intensive/143/673735.pdf?1570248858",
+  certificateJs1: "https://assets.htmlacademy.ru/certificates/intensive/121/673735.pdf?1571142862"
+};
