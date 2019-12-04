e359fe92b86d0c28d28bd082c60fdb086a51f367
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-04 23:25:41 +0300

    Adds lang data

---
+++ source/data/lang.js
@@ -0,0 +1,344 @@
+export const lang = {
+  ru: {
+    closeOpenMenu: "Открыть/закрыть меню",
+    russian: "Русский язык",
+    english: "Английский язык",
+    japanese: "Японский язык",
+    aboutMe: "Обо мне",
+    whatIUse: "Что я использую",
+    contacts: "Контакты",
+
+    title: "Портфолио",
+    profilePicture: "Аватар",
+    portfolioOfIK: "Портфолио Ирины Ковальчук",
+    myName: "Ирина Ковальчук",
+    myAges: "25 лет",
+    myLocation: "Фрязино, Россия",
+    myPosition: "Frontend разработчик",
+    technologies: "Технологии",
+    bem: "БЭМ",
+    crossBrowserMarkups: "Кросс-браузерная вёрстка (включая Internet Explorer 11)",
+    progressiveEnhancement: "Прогрессивное улучшение",
+    accessibility: "Доступность",
+    animation: "JS и CSS анимация",
+    portfolio: "Портфолио",
+    previewOfMarkup: "Предпросмотр вёрстки",
+    demo: "Демо",
+    sourceCode: "Исходный код",
+    description: "Описание",
+    availableGraphicEditors: "Доступные графические редакторы",
+    logotype: "логотип",
+    education: "Образование",
+    currentlyIStudyIn: "На данный момент я учусь в HTML Академии",
+    specialisation: "Специальность React-разработчик (плюс курсы PHP и Node.JS)",
+    hereIs: "Здесь ",
+    link: "ссылка ",
+    toMyStudentsAccount: "на мой студенческий профайл",
+    achievedCertification: "И полученные сертификаты",
+
+    pognali: {
+      title: "Погнали",
+      description: (
+        `<ul class='description-list'>
+          <li>Выпускная работа в HTML Academy(HTML & CSS уровень 2).</li>
+          <li>Адаптивная верстка</li>
+          <li>Чтобы пройти на другие страницы, нажмите на "Направления" и "Попутчики" в главном меню.</li>
+          <li>На главной странице нажмите "Показать тарифы для бизнеса" для открытия модального окна. По наведению на карточку попутчика, можно увидеть эффект + по наведению на флаг и транспорт.</li>
+          <li>На странице "Направления" нажмите "Выберите страну", чтобы открыть dropdown-меню.</li>
+          <li>На странице "Попутчики" фильтр по странам может быть развернут, жёлтый фильтр имеет возможность свернуть развернуть каждый фильтр + работают чекбоксы.</li>
+        </ul>`)
+    },
+    woodenCoyote: {
+      title: "Сайт-визитка Wooden Coyote",
+      description: (
+        `<ul class='description-list'>
+          <li>Адаптивная верстка.</li>
+          <li>Использование grid, @supports</li>
+          <li>Меню открывается по клику на кнопку-гамбургер.</li>
+        </ul>`)
+    },
+    seaCruises: {
+      title: "Морские круизы",
+      description: (
+        `<ul class='description-list'>
+          <li>Адаптивная верстка.</li>
+          <li>Для открытия модального окна, нажмите на заголовок любого круиза.</li>
+          <li>Меню открывается по клику на кнопку-гамбургер.</li>
+        </ul>`)
+    },
+    mishka: {
+      title: "Мишка",
+      description: (
+        `<ul class='description-list'>
+          <li>Выпускная работа в HTML Academy (HTML & CSS уровень 2).</li>
+          <li>Адаптивная верстка.</li>
+          <li>Чтобы открыть другие страницы используйте ссылки в главном меню "Каталог товаров" и "Вязание на заказ".</li>
+          <li>Для открытия модального окна, нажмите "Заказать" на главной странице или на иконку корзины на странице каталога.</li>
+          <li>Чекбоксы, радиокнопки работают на странице "Вязание на заказ".</li>
+        </ul>`)
+    },
+    pinch: {
+      title: "Pinch",
+      description: (
+        `<ul class='description-list'>
+          <li>Статичная верстка.</li>
+          <li>Сделана анимация слайдера.</li>
+        </ul>`)
+    },
+    gllacy: {
+      title: "Глэйси",
+      description: (
+        `<ul class='description-list'>
+          <li>Выпускная работа в HTML Academy (HTML & CSS уровень 1).</li>
+          <li>Статичная верстка.</li>
+          <li>На главной странице работает слайдер, интерактивная карта, модальное окно по нажатию внизу на кнопку "Форма обратной связи".</li>
+          <li>Сверстана страница каталога сливочного мороженого. В меню необходимо нажать "Каталог" -> "Сливочное".</li>
+          <li>Открываются доп. окна по наведению на иконку поиска, кнопку "Вход" и корзину в навигации.</li>
+        </ul>`)
+    },
+    device: {
+      title: "Device",
+      description: (
+        `<ul class='description-list'>
+          <li>Выпускная работа в HTML Academy (HTML & CSS уровень 1).</li>
+          <li>Статичная верстка.</li>
+          <li>На главной странице работают два слайдера (секция под header'ом и секция Доставка/Гарантия/Кредит), интерактивная карта появляется по нажатию на статичную, модальное окно - по нажатию внизу на кнопку \"Напишите нам\" (предусмотрена анимация ошибки – хотя бы одно поле не заполнено).</li>
+          <li>Сверстана страница каталога \"Моноподы для селфи\". В меню необходимо навести на \"Каталог товаров\" -> \"Моноподы для селфи\".</li>
+        </ul>`)
+    },
+
+    certificateHtmlCss1: "Профессиональный HTML & CSS уровень 1",
+    certificateHtmlCss2: "Профессиональный HTML & CSS уровень 2",
+    certificatePhp1: "Профессиональный PHP уровень 1",
+    certificateJs1: "Профессиональный JavaScript уровень 1",
+
+    certificate: "сертификат"
+  },
+
+  en: {
+    closeOpenMenu: "Close/open menu",
+    russian: "Russian",
+    english: "English",
+    japanese: "Japanese",
+    aboutMe: "About me",
+    whatIUse: "What I use",
+    contacts: "Contacts",
+
+    title: "Portfolio",
+    profilePicture: "Profile picture",
+    portfolioOfIK: "Portfolio of Irina Kovalchuk",
+    myName: "Irina Kovalchuk",
+    myAges: "25 y.o.",
+    myLocation: "Fryazino, Russia",
+    myPosition: "Frontend developer",
+    technologies: "Technologies",
+    bem: "BEM",
+    crossBrowserMarkups: "Cross-browser markups (including Internet Explorer 11)",
+    progressiveEnhancement: "Progressive enhancement",
+    accessibility: "Accessibility",
+    animation: "JS and CSS animation",
+    portfolio: "Portfolio",
+    previewOfMarkup: "Preview of markup",
+    demo: "Demo",
+    sourceCode: "Source code",
+    description: "Description",
+    availableGraphicEditors: "Available graphic editors",
+    logotype: "logotype",
+    education: "Education",
+    currentlyIStudyIn: "Currently I study in HTML Academy",
+    specialisation: "React-developer specialisation (plus PHP and Node.JS course)",
+    hereIs: "Here's the ",
+    link: "link ",
+    toMyStudentsAccount: "to my student's account",
+    achievedCertification: "And achieved certification",
+
+    pognali: {
+      title: "Pognali",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+          <li>Click "Направления" and "Попутчики" on the main menu to visit other pages.</li>
+          <li>Click "Показать тарифы для бизнеса" on the main page to open modal window. You can see the effect when hover on companion card + when hover on flag and transport.</li>
+          <li>Click "Выберите страну" on the page "Направления" to open dropdown-menu.</li>
+          <li>Filter by countries on the page "Попутчики" can be expanded, yellow filter has possibility to collapse and expand its every filter + work of the checkboxes.</li>
+        </ul>`)
+    },
+    woodenCoyote: {
+      title: "Landing page Wooden Coyote",
+      description: (
+        `<ul class='description-list'>
+          <li>Adaptive markup.</li>
+          <li>Using grid, @supports</li>
+          <li>Click hamburger-button to open menu.</li>
+        </ul>`)
+    },
+    seaCruises: {
+      title: "Sea cruises",
+      description: (
+        `<ul class='description-list'>
+          <li>Click on the header of any cruise card to open a modal window.</li>
+          <li>Click hamburger-button to open menu.</li>
+        </ul>`)
+    },
+    mishka: {
+      title: "Mishka",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+          <li>Click "Каталог товаров" and "Вязание на заказ" on the main menu to visit other pages.</li>
+          <li>Click "Заказать" on the main page or on the cart icon on the catalog page to open modal window.</li>
+          <li>Checkboxes, radiobuttons work on the page "Вязание на заказ".</li>
+        </ul>`)
+    },
+    pinch: {
+      title: "Pinch",
+      description: (
+        `<ul class='description-list'>
+          <li>Static markup.</li>
+          <li>Slide-show animation.</li>
+        </ul>`)
+    },
+    gllacy: {
+      title: "Gllacy",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+          <li>Slide-show, interactive map work on the main page, click "Форма обратной связи" on the bottom of the page to open modal window.</li>
+          <li>Click "Каталог" -> "Сливочное" to open another page.</li>
+          <li>Hover search icon, button "Вход" or cart in navigation to open additional windows.</li>
+        </ul>`)
+    },
+    device: {
+      title: "Device",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+          <li>There are two slide-shows on the main page (section under header'ом and section Доставка/Гарантия/Кредит), click on the static map to open interactive one, click on "Напишите нам" to open modal window (send form without information to see the errors).</li>
+          <li>Hover "Каталог товаров" -> "Моноподы для селфи" to open another page.</li>
+        </ul>`)
+    },
+
+    certificateHtmlCss1: "Professional HTML & CSS level 1",
+    certificateHtmlCss2: "Professional HTML & CSS level 2",
+    certificatePhp1: "Professional PHP level 1",
+    certificateJs1: "Professional JavaScript level 1",
+
+    certificate: "certificate"
+  },
+
+  ja: {
+    closeOpenMenu: "メニューを閉じる/開く",
+    russian: "ロシア語",
+    english: "英語",
+    japanese: "日本語",
+    aboutMe: "私について",
+    whatIUse: "テクノロジズ",
+    contacts: "連絡先",
+
+    title: "ポートフォリオ",
+    profilePicture: "プロフィールの写真",
+    portfolioOfIK: "イリナ・コバルチュクのポートフォリオ",
+    myName: "イリナ・コバルチュク",
+    myAges: "２５歳",
+    myLocation: "フリャジノ, ロシア",
+    myPosition: "ウェブ開発者",
+    technologies: "テクノロジズ",
+    bem: "BEM",
+    crossBrowserMarkups: "クロスブラウザマークアップ(Internet Explorer 11を含む)",
+    progressiveEnhancement: "プログレッシブエンハンスメント",
+    accessibilit: "アクセシビリティ",
+    animation: "JSとCSSのアニメーション",
+    portfolio: "ポートフォリオ",
+    previewOfMarkup: "マークアップのプレビュー",
+    demo: "デモ",
+    sourceCode: "ソースコード",
+    description: "説明",
+    availableGraphicEditors: "グラフィックエディタ",
+    logotype: "ロゴタイプ",
+    education: "教育",
+    currentlyIStudyIn: "現在、HTML Academyで勉強しています",
+    specialisation: "React-開発者の専門化(PHPおよびNode.JSコース)",
+    hereIs: "これが生徒アカウントへの",
+    link: "リンク",
+    toMyStudentsAccount: "です",
+    achievedCertification: "認定",
+
+    pognali: {
+      title: "ポニャリ",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+          <li>Responsive markup.</li>
+          <li>Click "Направления" and "Попутчики" on the main menu to visit other pages.</li>
+          <li>Click "Показать тарифы для бизнеса" on the main page to open modal window. You can see the effect when hover on companion card + when hover on flag and transport.</li>
+          <li>Click "Выберите страну" on the page "Направления" to open dropdown-menu.</li>
+          <li>Filter by countries on the page "Попутчики" can be expanded, yellow filter has possibility to collapse and expand its every filter + work of the checkboxes.</li>
+        </ul>`)
+    },
+    woodenCoyote: {
+      title: "ランディングページ Wooden Coyote",
+      description: (
+        `<ul class='description-list'>
+          <li>Adaptive markup.</li>
+          <li>Using grid, @supports</li>
+          <li>Click hamburger-button to open menu.</li>
+        </ul>`)
+    },
+    seaCruises: {
+      title: "海のクルーズ",
+      description: (
+        `<ul class='description-list'>
+          <li>Responsive markup.</li>
+          <li>Click on the header of any cruise card to open a modal window.</li>
+          <li>Click hamburger-button to open menu.</li>
+        </ul>`)
+    },
+    mishka: {
+      title: "ミシュカ",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+          <li>Responsive markup.</li>
+          <li>Click "Каталог товаров" and "Вязание на заказ" on the main menu to visit other pages.</li>
+          <li>Click "Заказать" on the main page or on the cart icon on the catalog page to open modal window.</li>
+          <li>Checkboxes, radiobuttons work on the page "Вязание на заказ".</li>
+        </ul>`)
+    },
+    pinch: {
+      title: "Pinch",
+      description: (
+        `<ul class='description-list'>
+          <li>Static markup.</li>
+          <li>Slide-show animation.</li>
+        </ul>`)
+    },
+    gllacy: {
+      title: "グラシー",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+          <li>Static markup.</li>
+          <li>Slide-show, interactive map work on the main page, click "Форма обратной связи" on the bottom of the page to open modal window.</li>
+          <li>Click "Каталог" -> "Сливочное" to open another page.</li>
+          <li>Hover search icon, button "Вход" or cart in navigation to open additional windows.</li>
+        </ul>`)
+    },
+    device: {
+      title: "デバイス",
+      description: (
+        `<ul class='description-list'>
+          <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+          <li>Static markup.</li>
+          <li>There are two slide-shows on the main page (section under header'ом and section Доставка/Гарантия/Кредит), click on the static map to open interactive one, click on "Напишите нам" to open modal window (send form without information to see the errors).</li>
+          <li>Hover "Каталог товаров" -> "Моноподы для селфи" to open another page.</li>
+        </ul>`)
+    },
+
+    certificateHtmlCss1: "プロフェッショナルなHTMLおよびCSSレベル1",
+    certificateHtmlCss2: "プロフェッショナルなHTMLおよびCSSレベル２",
+    certificatePhp1: "プロフェッショナルPHPレベル1",
+    certificateJs1: "プロフェッショナルJavaScriptレベル1",
+
+    certificate: "認定"
+  }
+};
