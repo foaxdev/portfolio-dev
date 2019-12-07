4c559baaf29ec7ad8ea8846eb7566d8a2d10f537
イリーナ・コバルチュク <foaxdev@yahoo.com>
2019-12-07 23:39:01 +0300

    Removes lang from portfolio-card.js

--- source/components/portfolio-card.js
+++ source/components/portfolio-card.js
@@ -1,20 +1,19 @@
 import {createElement} from "../utils";
-import {lang} from "../data/lang";
 
 const createCardTemplate = cardData => (
   `<li class="markups-list__item">
     <picture>
       <source type="image/webp" srcset="img/portfolio/preview-${cardData.alias}.webp">
 
-      <img class="markups-list__image" src="img/portfolio/preview-${cardData.alias}.jpg" alt="<?=$lang[$language]["previewOfMarkup"];?> <?=$lang[$language][$project_name]["title"];?>" width="270" height="199">
+      <img class="markups-list__image" src="img/portfolio/preview-${cardData.alias}.jpg" alt="Preview of markup ${cardData.title}" width="270" height="199">
     </picture>
     <div class="markups-list__links-wrap">
-      <a class="markups-list__link" href="${cardData.prodLink}">${lang.en.demo}</a>
-      <a class="markups-list__link" href="${cardData.sourceLink}">${lang.en.sourceCode}</a>
+      <a class="markups-list__link" href="${cardData.prodLink}">Demo</a>
+      <a class="markups-list__link" href="${cardData.sourceLink}">Source code</a>
     </div>
     <div class="markups-list__description-wrap">
-      <button class="markups-list__description-button js-description-btn" type="button">${lang.en.description}</button>
-      <div class="markups-list__description markups-list__description--hidden"><?=$lang[$language][$project_name]["description"];?></div>
+      <button class="markups-list__description-button js-description-btn" type="button">Description</button>
+      <div class="markups-list__description markups-list__description--hidden">${cardData.description}</div>
     </div>
   </li>`
 );
--- source/data/portfolio.js
+++ source/data/portfolio.js
@@ -2,36 +2,87 @@ export const portfolio = {
   pognali: {
     sourceLink: "https://github.com/foaxdev/pognali-dev",
     prodLink: "https://foaxdev.github.io/pognali/",
-    alias: "pognali"
+    alias: "pognali",
+    title: "Pognali",
+    description: (
+      `<ul class='description-list'>
+        <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+        <li>Click "Направления" and "Попутчики" on the main menu to visit other pages.</li>
+        <li>Click "Показать тарифы для бизнеса" on the main page to open modal window. You can see the effect when hover on companion card + when hover on flag and transport.</li>
+        <li>Click "Выберите страну" on the page "Направления" to open dropdown-menu.</li>
+        <li>Filter by countries on the page "Попутчики" can be expanded, yellow filter has possibility to collapse and expand its every filter + work of the checkboxes.</li>
+      </ul>`)
   },
   woodenCoyote: {
     sourceLink: "https://github.com/foaxdev/wooden-coyote-dev",
     prodLink: "https://wooden-coyote.site",
-    alias: "oden-coyote"
+    alias: "oden-coyote",
+    title: "Landing page Wooden Coyote",
+    description: (
+      `<ul class='description-list'>
+        <li>Adaptive markup.</li>
+        <li>Using grid, @supports</li>
+        <li>Click hamburger-button to open menu.</li>
+      </ul>`)
   },
   seaCruises: {
     sourceLink: "https://github.com/foaxdev/sea-cruises-dev",
     prodLink: "https://foaxdev.github.io/sea-cruises/",
-    alias: "sea-cruises"
+    alias: "sea-cruises",
+    title: "Sea cruises",
+    description: (
+      `<ul class='description-list'>
+        <li>Click on the header of any cruise card to open a modal window.</li>
+        <li>Click hamburger-button to open menu.</li>
+      </ul>`)
   },
   mishka: {
     sourceLink: "https://github.com/foaxdev/mishka-dev",
     prodLink: "https://foaxdev.github.io/mishka/",
-    alias: "mishka"
+    alias: "mishka",
+    title: "Mishka",
+    description: (
+      `<ul class='description-list'>
+        <li>Graduation project in HTML Academy (HTML & CSS level 2).</li>
+        <li>Click "Каталог товаров" and "Вязание на заказ" on the main menu to visit other pages.</li>
+        <li>Click "Заказать" on the main page or on the cart icon on the catalog page to open modal window.</li>
+        <li>Checkboxes, radiobuttons work on the page "Вязание на заказ".</li>
+      </ul>`)
   },
   pinch: {
     sourceLink: "https://github.com/foaxdev/pinch-dev",
     prodLink: "https://foaxdev.github.io/pinch/",
-    alias: "pinch"
+    alias: "pinch",
+    title: "Pinch",
+    description: (
+      `<ul class='description-list'>
+        <li>Static markup.</li>
+        <li>Slide-show animation.</li>
+      </ul>`)
   },
   gllacy: {
     sourceLink: "https://github.com/foaxdev/gllacy-dev",
     prodLink: "https://foaxdev.github.io/gllacy/",
-    alias: "gllacy"
+    alias: "gllacy",
+    title: "Gllacy",
+    description: (
+      `<ul class='description-list'>
+        <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+        <li>Slide-show, interactive map work on the main page, click "Форма обратной связи" on the bottom of the page to open modal window.</li>
+        <li>Click "Каталог" -> "Сливочное" to open another page.</li>
+        <li>Hover search icon, button "Вход" or cart in navigation to open additional windows.</li>
+      </ul>`)
   },
   device: {
     sourceLink: "https://github.com/foaxdev/device-dev",
     prodLink: "https://foaxdev.github.io/device",
-    alias: "device"
+    alias: "device",
+    title: "Device",
+    description: (
+      `<ul class='description-list'>
+        <li>Graduation project in HTML Academy (HTML & CSS level 1).</li>
+        <li>There are two slide-shows on the main page (section under header'ом and section Доставка/Гарантия/Кредит), click on the static map to open interactive one, click on "Напишите нам" to open modal window (send form without information to see the errors).</li>
+        <li>Hover "Каталог товаров" -> "Моноподы для селфи" to open another page.</li>
+      </ul>`)
   }
 };
